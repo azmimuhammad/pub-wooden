4c605527946f9473a3268bc7c69576d7bb0096d4
Azmi Muhammad <azmim85@gmail.com>
2022-10-10 22:57:27 +0700

    feat: add FAB & title & icon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Fab} from '@mui/material'
 import {Routes, Route} from 'react-router-dom'
 import RouterConfig from '../config/Router.config'
 import {Container} from '../config/Global.styles'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 
 const HomePage = () => {
   return (
@@ -18,6 +20,23 @@ const HomePage = () => {
         </Routes>
       </Container>
       <Footer />
+      <Fab
+        component="a"
+        href="https://bit.ly/ChatAzmi"
+        target="_blank"
+        aria-label="add"
+        style={{
+          backgroundColor: '#25D366',
+          margin: 0,
+          top: 'auto',
+          right: 20,
+          bottom: 20,
+          left: 'auto',
+          position: 'fixed',
+        }}
+      >
+        <WhatsAppIcon />
+      </Fab>
     </div>
   )
 }
